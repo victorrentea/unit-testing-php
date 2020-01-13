de59a25e671cb87a91311fb21e3712f18723c905
Victor Rentea <victorrentea@gmail.com>
2020-01-13 09:07:16 +0200

    test

---
+++ tests/Test1.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace PhpUnitWorkshopTest;
+
+
+use PHPUnit\Framework\TestCase;
+
+class Test1 extends TestCase
+{
+    /** @test */
+    public function m() {
+        self::assertTrue(true);
+    }
+
+}
