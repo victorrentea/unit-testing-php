e0871087e673fbb51497b253c1e2688dd99c575e
Victor Rentea <victorrentea@gmail.com>
2023-02-08 16:56:41 +0200

    testu e mai expresiv cand pica

--- tests/mocks/CreateConfigTest.php
+++ tests/mocks/CreateConfigTest.php
@@ -7,13 +7,19 @@ use PHPUnit\Framework\TestCase;
 
 class CreateConfigTest extends TestCase
 {
-//
+
+    const UNIQUEID_LENGTH = 13;
+
     function test1(): void
     {
         $clientMock = $this->createMock(TelemetryClient::class);
         $target = new TelemetryDiagnosticControls($clientMock);
         $config = $target->createConfiguration(new ClientVersion(1,0));
-        self::assertMatchesRegularExpression('#1.0-\w{13}$#', $config->getSessionId());
+
+        //        self::assertMatchesRegularExpression('#1-\w{13}$#', $config->getSessionId());
+
+        self::assertEquals(strlen('1.0-') + self::UNIQUEID_LENGTH, strlen($config->getSessionId()));
+        self::assertStringStartsWith('1.0-', $config->getSessionId());
         echo $config->getSessionId();
         TestUtils::assertJustNow($config->getSessionStart());
         self::assertEquals(TelemetryClientConfiguration::ACK_NORMAL, $config->getAckMode()); // ruleaza la linia 100
